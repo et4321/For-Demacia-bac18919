408ed81d66c5b82b46c5a5a7ece6f49e19818822
et4321 <54795466+et4321@users.noreply.github.com>
2019-10-09 11:30:11 +0200

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,13 @@
+<?php
+$klas = array(
+    "sjonnie de wiel",
+    "herman kaal",
+    "henk de steen",
+    "inge kerkhoven",
+    "gert kruiswijk",
+);
+
+for ($i = 0; $i <= count ($klas); $i++) { 
+    echo $klas[$i]; 
+}
+?>
